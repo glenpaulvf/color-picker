f6502980bff1d8cb5482c5362e46dfb62fa52524
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-29 15:44:55 -0700

    Enable color viewer to be draggable

--- colorpicker.py
+++ colorpicker.py
@@ -8,17 +8,29 @@ class ColorPickerWindow(QWidget):
 
     def __init__(self):
         super(ColorPickerWindow, self).__init__()
+        
+        # Window properties
+        self.width = 600
+        self.height = 400
+        
         self.init_UI()
     
     def init_UI(self):
         # Set window dimensions to 600px by 400px
-        self.resize(600, 400)
+        self.resize(self.width, self.height)
         
         # Set window background color to white
         pal = self.palette()
         pal.setColor(self.backgroundRole(), Qt.white)
         self.setAutoFillBackground(True)
         self.setPalette(pal)
+
+        # Initialize color viewer        
+        viewer = ColorViewer(self)
+        viewer.resize(50, 50) # force viewer dimensions to 50px square
+        
+        
+class ColorViewer(QWidget):
         
     def paintEvent(self, event):
         painter = QPainter()
@@ -31,9 +43,31 @@ class ColorPickerWindow(QWidget):
         painter.setBrush(Qt.red)
         painter.drawRect(10, 10, 50, 50)
 
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.press_pos = event.globalPos()
+            self.move_pos = event.globalPos()
+
+        super(ColorViewer, self).mousePressEvent(event)
+    
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            # Adjust offset from clicked point to origin of widget
+            curr_pos = self.mapToGlobal(self.pos())
+            global_pos = event.globalPos()
+            offset = global_pos - self.move_pos
+            new_pos = self.mapFromGlobal(curr_pos + offset)
+            self.move(new_pos)
+            self.move_pos = global_pos
+
+        super(ColorViewer, self).mouseMoveEvent(event)
+    
+    def mouseReleaseEvent(self, event):
+        super(ColorViewer, self).mouseReleaseEvent(event)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    cpw = ColorPickerWindow()
-    cpw.show()
+    cv = ColorPickerWindow()
+    cv.show()
     app.exec_()
