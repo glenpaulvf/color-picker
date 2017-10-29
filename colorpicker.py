771d5c09dd376d9aee46fd140893b992d32df5cf
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-29 00:57:34 -0700

    Draw square color viewer

--- colorpicker.py
+++ colorpicker.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter
 from PyQt5.QtCore import Qt
 
 
@@ -18,6 +19,17 @@ class ColorPickerWindow(QWidget):
         pal.setColor(self.backgroundRole(), Qt.white)
         self.setAutoFillBackground(True)
         self.setPalette(pal)
+        
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        self.draw_viewer(painter)
+        painter.end()
+    
+    def draw_viewer(self, painter):
+        painter.setPen(Qt.black)
+        painter.setBrush(Qt.white)
+        painter.drawRect(10, 10, 50, 50)
 
 
 if __name__ == '__main__':
